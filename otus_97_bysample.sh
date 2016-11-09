69a76b3415fa0a1e466c09e34e9c2d8871a03940
Ross Cunning <ross.cunning@gmail.com>
2016-11-09 12:46:52 -0800

    get samplenames from filenames

--- otus_97_bysample.sh
+++ otus_97_bysample.sh
@@ -6,7 +6,7 @@ split_sequence_file_on_sample_ids.py -i data/fasta/combined_seqs_trimmed.fasta -
 
 # OTU clustering at 97% similarity for each sample (in parallel)
 clust97() {
-	s=`echo $1 | grep -Eoh '/[0-9]{1,4}' | cut -c 2-`
+	s=$(basename $1 | cut -d. -f1)
 	pick_otus.py -i $1 -s 0.97 --denovo_otu_id_prefix $s'_denovo' --optimal_uclust -o data/otus_97_bysample
 }
 export -f clust97
@@ -14,15 +14,18 @@ parallel clust97 ::: data/fasta/bysample/*.fasta
 
 # Pick representative sequence set for each sample
 repset97() {
-	s=`echo $1 | grep -Eoh '/[0-9]{1,4}' | cut -c 2-`
+	s=$(basename $1 | cut -d. -f1)
 	pick_rep_set.py -i $1 -m most_abundant -f 'data/fasta/bysample/'$s'.fasta' \
 	-o 'data/otus_97_bysample/'$s'_rep_set.fasta'
 }
 export -f repset97
-parallel repset97 ::: data/otus_97_bysample/[0-9]*_otus.txt
+rm -f data/otus_97_bysample/all_rep_set_otus.txt  # remove all_rep_set_otus if it already exists
+parallel repset97 ::: data/otus_97_bysample/*_otus.txt
 
 # Merge all rep sets together
-cat data/otus_97_bysample/[0-9]*_rep_set.fasta > data/otus_97_bysample/all_rep_set.fasta
+rm -f data/otus_97_bysample/all_rep_set.fasta  # remove all_rep_set if it already exists
+rm -f data/otus_97_bysample/all_rep_set_rep_set.fasta  # remove all_rep_set_rep_set if it already exists
+cat data/otus_97_bysample/*_rep_set.fasta > data/otus_97_bysample/all_rep_set.fasta
 
 # Cluster all 97% rep sets at 100% identity
 pick_otus.py -i data/otus_97_bysample/all_rep_set.fasta -s 1.0 --optimal_uclust -o data/otus_97_bysample
