5673c61fd2c4bc3abd0963c654133c789c6b03fd
Ross Cunning <ross.cunning@gmail.com>
2016-11-09 11:00:36 -0800

    separate merge/qc_trim

--- qc_trim_reads.sh
+++ qc_trim_reads.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-# Generate config files for merging reads with illumina-utils
-mkdir -p data/merge
-cp data/fastq_list.txt data/fastq/fastq_list.txt
-iu-gen-configs data/fastq/fastq_list.txt -o data/merge
-
-# Merge paired reads for each sample (in parallel)
-parallel iu-merge-pairs {} --min-overlap-size 150 --enforce-Q30-check --marker-gene-stringent ::: data/merge/*.ini
-
-# Filter sequences - keep only those with 3 mismatches or less (in parallel)
-parallel iu-filter-merged-reads {} --max-mismatches 3 ::: data/merge/*_MERGED
-
 # Add QIIME labels
 add_qiime_labels.py -m data/mapping_file.txt -i data/merge/ -c InputFileName -o data/fasta
 
