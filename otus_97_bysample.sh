e586b0e9512e60eb6a9308e9c0436b794885c733
Ross Cunning <ross.cunning@gmail.com>
2016-11-18 13:15:28 -1000

    generalize directory structure

--- otus_97_bysample.sh
+++ otus_97_bysample.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
+# Takes input arguments: 1=path to input fasta file; 2=output directory path
+
 # Split into individual sample files
-rm -rf data/fasta/bysample
-split_sequence_file_on_sample_ids.py -i data/fasta/combined_seqs_trimmed.fasta -o data/fasta/bysample
+rm -rf $2
+split_sequence_file_on_sample_ids.py -i $1 -o $2
 
 # OTU clustering at 97% similarity for each sample (in parallel)
 clust97() {
 	s=$(basename $1 | cut -d. -f1)
-	pick_otus.py -i $1 -s 0.97 --denovo_otu_id_prefix $s'_denovo' --optimal_uclust -o data/otus_97_bysample
+	dir=$(dirname $1)
+	pick_otus.py -i $1 -s 0.97 --denovo_otu_id_prefix $s'_denovo' --optimal_uclust -o $dir
 }
 export -f clust97
-parallel clust97 ::: data/fasta/bysample/*.fasta
+parallel clust97 ::: $2/*.fasta
 
 # Pick representative sequence set for each sample
 repset97() {
-	s=$(basename $1 | cut -d. -f1)
-	pick_rep_set.py -i $1 -m most_abundant -f 'data/fasta/bysample/'$s'.fasta' \
-	-o 'data/otus_97_bysample/'$s'_rep_set.fasta'
+	s=$(basename $1 | cut -d_ -f1)
+	dir=$(dirname $1)
+	pick_rep_set.py -i $1 -m most_abundant -f $dir/$s'.fasta' -o $dir/$s'_rep_set.fasta'
 }
 export -f repset97
-rm -f data/otus_97_bysample/all_rep_set_otus.txt  # remove all_rep_set_otus if it already exists
-parallel repset97 ::: data/otus_97_bysample/*_otus.txt
+rm -f $2/all_rep_set_otus.txt  # remove all_rep_set_otus if it already exists
+parallel repset97 ::: $2/*_otus.txt
 
 # Merge all rep sets together
-rm -f data/otus_97_bysample/all_rep_set.fasta  # remove all_rep_set if it already exists
-rm -f data/otus_97_bysample/all_rep_set_rep_set.fasta  # remove all_rep_set_rep_set if it already exists
-cat data/otus_97_bysample/*_rep_set.fasta > data/otus_97_bysample/all_rep_set.fasta
+rm -f $2/all_rep_set.fasta  # remove all_rep_set if it already exists
+rm -f $2/all_rep_set_rep_set.fasta  # remove all_rep_set_rep_set if it already exists
+cat $2/*_rep_set.fasta > $2/all_rep_set.fasta
 
 # Cluster all 97% rep sets at 100% identity
-pick_otus.py -i data/otus_97_bysample/all_rep_set.fasta -s 1.0 --optimal_uclust -o data/otus_97_bysample
+pick_otus.py -i $2/all_rep_set.fasta -s 1.0 --optimal_uclust -o $2
 
 # Get rep set of 100% OTUs
-pick_rep_set.py -i data/otus_97_bysample/all_rep_set_otus.txt -f data/otus_97_bysample/all_rep_set.fasta \
--o data/otus_97_bysample/all_rep_set_rep_set.fasta
+pick_rep_set.py -i $2/all_rep_set_otus.txt -f $2/all_rep_set.fasta -o $2/all_rep_set_rep_set.fasta
 
 # Concatenate 97% OTU maps and merge with 100% OTU map
-cat data/otus_97_bysample/*_otus.txt > data/otus_97_bysample/all_97_otus.txt
-merge_otu_maps.py -i data/otus_97_bysample/all_97_otus.txt,data/otus_97_bysample/all_rep_set_otus.txt -o data/otus_97_bysample/merged_otu_map.txt
+cat $2/*_otus.txt > $2/all_97_otus.txt
+merge_otu_maps.py -i $2/all_97_otus.txt,$2/all_rep_set_otus.txt -o $2/merged_otu_map.txt
 
 # Remove singletons
-awk '$3 ~ /./ {print}' data/otus_97_bysample/merged_otu_map.txt > data/otus_97_bysample/nosingles_otus.txt
+awk '$3 ~ /./ {print}' $2/merged_otu_map.txt > $2/nosingles_otus.txt
 
 # Make OTU table
-make_otu_table.py -i data/otus_97_bysample/nosingles_otus.txt -o data/otus_97_bysample/97_otus_bysample.biom
-rm -f  data/otus_97_bysample/97_otus_bysample.tsv  # delete old OTU .tsv if present
-biom convert -i data/otus_97_bysample/97_otus_bysample.biom -o data/otus_97_bysample/97_otus_bysample.tsv --to-tsv
+make_otu_table.py -i $2/nosingles_otus.txt -o $2/97_otus_bysample.biom
+rm -f  $2/97_otus_bysample.tsv  # delete old OTU .tsv if present
+biom convert -i $2/97_otus_bysample.biom -o $2/97_otus_bysample.tsv --to-tsv
