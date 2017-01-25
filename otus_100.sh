cd51b671d35e53d7b1cf05530f35e2a176476d77
daniclaar <danielle.claar@gmail.com>
2017-01-25 11:28:54 -0800

    switching awk to gawk for use with large data sets

--- otus_100.sh
+++ otus_100.sh
@@ -4,7 +4,7 @@
 pick_otus.py -i $1 -s 1.0 -o $2
 
 # Remove singletons before picking rep set and assigning taxonomy (to save time)
-awk '$3 ~ /./ {print}' $2/$3 > $2/nosingles_otus.txt
+gawk '$3 ~ /./ {print}' $2/$3 > $2/nosingles_otus.txt
 
 # Get rep set of 100% OTUs
 pick_rep_set.py -i $2/nosingles_otus.txt \
--- otus_97.sh
+++ otus_97.sh
@@ -4,7 +4,7 @@
 pick_otus.py -i data/fasta/combined_seqs_trimmed.fasta -s 0.97 --optimal_uclust -o data/otus_97/
 
 # Remove singletons before picking rep set and assigning taxonomy (to save time)
-awk '$3 ~ /./ {print}' data/otus_97/combined_seqs_trimmed_otus.txt > data/otus_97/nosingles_otus.txt
+gawk '$3 ~ /./ {print}' data/otus_97/combined_seqs_trimmed_otus.txt > data/otus_97/nosingles_otus.txt
 
 # Get rep set of 97% OTUs
 pick_rep_set.py -i data/otus_97/nosingles_otus.txt \
