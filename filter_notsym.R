5ef5173c870aeb642ca6094f93096cffa615512d
Ross Cunning <ross.cunning@gmail.com>
2017-03-08 20:59:33 -0500

    blast to local nt as arg

--- filter_notsym.R
+++ filter_notsym.R
@@ -10,7 +10,8 @@ if (length(args) < 3) {
   stop("must specify 
        1: phyloseq object (.RData); 
        2: sequences; 
-       3: output filename", call.=FALSE)
+       3: output filename
+       4: local nt blast database", call.=FALSE)
 }
 
 # Get tax data from phyloseq object
@@ -29,12 +30,42 @@ poorseqsfile <- file.path(dirname(args[2]), "poorseqs.fasta")
 writeXStringSet(poorseqs, filepath=poorseqsfile, append=FALSE,
                 compress=FALSE, compression_level=NA, format="fasta")
 
-# Blast poor matching sequences to NCBI nr databse, get top hit
+
+
+
+
+# Blast poor matching sequences to local NCBI nt databse, get top hit, write to file
 outfile <- file.path(dirname(args[2]), "poorseqs_blast_results.txt")
-system(paste("blastn -db nr -remote -query",
-              poorseqsfile,
-              "-outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u -k1,1 >", 
-              outfile))
+system(paste("blastn -db", args[4],
+            "-query", poorseqsfile, "-num_threads 100",
+            "-outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u -k1,1 >",
+            outfile))
+
+# Split poor-matching sequences into files with 100 seqs each for blasting to nt database
+# poorseqs <- readDNAStringSet("~/Documents/Academia/HIMB/USVI/STJ2015/data/otus_97_bysample/poorseqs.fasta")
+# poorseqsbins <- split(poorseqs, cut(1:length(poorseqs), breaks=length(poorseqs) %/% 100 + 1, labels=seq(1,length(poorseqs) %/% 100 + 1)))
+# sapply(names(poorseqsbins), function(x) {
+#   writeXStringSet(poorseqsbins[[x]], filepath = file.path(dirname(args[2]), paste0("poorseqs", x,".fasta")))
+# })
+#
+# outfile <- file.path(dirname(args[2]), "poorseqs_blast_results.txt")
+# system(paste("rm", outfile))
+# poorseqsfiles <- list.files(path=file.path(dirname(args[2])), pattern="^poorseqs[0-9]", full.names=T)
+# 
+# library(parallel) # Set up for parallel processing
+# cl <- makeCluster(detectCores())  # Initiate cluster
+# clusterEvalQ(cl, library(Biostrings))  # Make Biostrings library available to cluster
+# clusterExport(cl, c("poorseqsfiles", "outfile"))  # Export objects to cluster
+# 
+# parSapply(cl, poorseqsfiles, function(x) {
+#   system(paste("blastn -db", args[4],
+#                "-query", x,
+#                "-outfmt '6 qseqid qcovs stitle sseqid' -max_target_seqs 1 | sort -u -k1,1 >>",
+#                outfile))
+# })
+# 
+# stopCluster(cl)  # Stop cluster
+
 
 # Read BLAST results
 poorseqs_blast <- readLines(outfile)
