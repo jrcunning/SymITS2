321b87d33e218852580c474ee3163a327fd9143a
Ross Cunning <ross.cunning@gmail.com>
2017-03-16 14:32:56 -0400

    generalize code

--- otus_97.sh
+++ otus_97.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Arguments: 1: input fasta file
+#            2: output directory
+
+# Delete output directory if it exists to avoid recursion
+rm -rf $2
+
 # Cluster entire dataset at 97% similarity
-pick_otus.py -i data/fasta/combined_seqs_trimmed.fasta -s 0.97 --optimal_uclust -o data/otus_97/
+# Cluster entire dataset at 100% similarity
+pick_otus.py -i $1 -s 0.97 -o $2
 
 # Remove singletons before picking rep set and assigning taxonomy (to save time)
-gawk '$3 ~ /./ {print}' data/otus_97/combined_seqs_trimmed_otus.txt > data/otus_97/nosingles_otus.txt
+gawk '$3 ~ /./ {print}' $2/*_otus.txt > $2/nosingles_otus.txt
 
 # Get rep set of 97% OTUs
-pick_rep_set.py -i data/otus_97/nosingles_otus.txt \
--f data/fasta/combined_seqs_trimmed.fasta \
--o data/otus_97/97_otus_rep_set.fasta
+pick_rep_set.py -i $2/nosingles_otus.txt \
+-f $1 \
+-o $2/97_otus_rep_set.fasta
 
 # Make OTU table
-make_otu_table.py -i data/otus_97/nosingles_otus.txt -o data/otus_97/97_otus.biom
-rm -f  data/otus_97/97_otus.tsv  # delete old OTU .tsv if present
-biom convert -i data/otus_97/97_otus.biom -o data/otus_97/97_otus.tsv --to-tsv
+make_otu_table.py -i $2/nosingles_otus.txt -o $2/97_otus.biom
+rm -f  $2/97_otus.tsv  # delete old OTU .tsv if present
+biom convert -i $2/97_otus.biom -o $2/97_otus.tsv --to-tsv
